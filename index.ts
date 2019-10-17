cecfd63527c3691c0fc71b74532c45fe34e56c62
Yasin Malli <yasinmalli@gmail.com>
2019-10-16 23:58:50 -0700

    some changes.

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import { Node } from "./src/helpers/Classes";
 
 let node: Node<string> = new Node("test");
+node.appendToTail("test2");
+node.appendToTail("test3");
+let newNode: Node<string> = node.deleteNode(node, "test2");
 
-console.log(node.data);
+while(newNode != null) {
+    console.log(newNode.data);
+    newNode = newNode.next;
+}
--- src/helpers/Classes.ts
+++ src/helpers/Classes.ts
@@ -8,10 +8,34 @@ export class Node<T> {
     }
 
     appendToTail(d: T): void {
+        let currentNode: Node<T> = this;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
 
+        currentNode.next = new Node<T>(d);
+    }
+
+    deleteNode(head: Node<T>, d: T): Node<T> {
+        let currentNode: Node<T> = head;
+
+        if (currentNode.data == d) {
+            return currentNode.next;
+        }
+
+        while (currentNode.next != null) {
+            if (currentNode.next.data == d) {
+                currentNode.next = currentNode.next.next;
+                return head;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return head;
     }
 }
 
 export class LinkedList<T> {
     head: Node<T>;
-}
+}   
